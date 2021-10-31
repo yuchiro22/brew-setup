866322e2d0f4bf0d11b13409cb97f29d65f1c9b0
Yuichiro Tsuji <t.yuyu.alies@gmail.com>
2021-10-31 11:34:51 +0900

    initial commit

---
+++ brew-setup.sh
@@ -0,0 +1,13 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install --cask iterm2
+brew install --cask docker
+brew install --cask discord
+brew install --cask slack
+## needs for google-japanse-ime
+sudo softwareupdate --install-rosetta
+brew install --cask google-japanese-ime
+brew install --cask visual-studio-code
+brew install --cask clipy
+brew install --cask zoom
+brew install --cask google-chrome
+
